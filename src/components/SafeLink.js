98fcc596e6d5179ce05c3bf1d586071be0d8d597
Yomguithereal <guillaumeplique@gmail.com>
2019-07-01 18:30:22 +0200

    Sidenotes

---
+++ src/components/SafeLink.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SafeLink({children, href}) {
+  return (
+    <a href={href} target="_blank" rel="noopener">
+      {children}
+    </a>
+  );
+}
--- src/components/tufte.js
+++ src/components/tufte.js
@@ -8,6 +8,16 @@ export function MarginNote({children}) {
   );
 }
 
+export function SideNote({id, children}) {
+  return (
+    <>
+      <label className="margin-toggle sidenote-number" for={id} />
+      <input type="checkbox" id={id} className="margin-toggle" />
+      <span className="sidenote">{children}</span>
+    </>
+  );
+}
+
 export function Divider() {
   return (
     <p style={{textAlign: 'center', fontSize: '24px', margin: '40px 0px'}}>
